b155cbd87053e42b86646a4dbfb7fe538154d0cd
Marek Nohaj <marek.nohaj.ke@gmail.com>
2026-04-24 15:25:08 +0200

    Fix API URL to Railway backend

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "690693650" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1473161022" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
